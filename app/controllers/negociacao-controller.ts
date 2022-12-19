dcb2ebadd3f8f4be5d772500bec0817b728aa3b3
Cleber Guimarães da Silva <cleberg83@outlook.com>
2022-12-19 18:47:39 -0300

    Teste

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,10 +1,12 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController{
 
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes: Negociacoes = new Negociacoes();
 
     constructor() {
 
@@ -17,21 +19,18 @@ export class NegociacaoController{
     adiciona(): void{
         
         const negociacao = this.criaNegociacao(); 
-        
-        console.log( negociacao );
-
+        this.negociacoes.adiciona(negociacao);                
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
 
     }
 
     criaNegociacao(): Negociacao{
 
-        const exp = /-/g;        
-        
+        const exp = /-/g;                
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-                
+        const valor = parseFloat(this.inputValor.value);                
         const negociacao = new Negociacao(date, quantidade, valor)
         
         return new Negociacao(date, quantidade, valor);
